bf25203f9fec6f3fa4483293f34a5b14aac2b5e2
ArkanKovanAbdullah <79310452+ArkanKovanAbdullah@users.noreply.github.com>
2022-10-21 22:08:11 +0300

    Linter errors

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,13 +1,10 @@
 
-const burgurIcon = document.querySelector(".burgur-icon");
-const navMenu = document.querySelector(".nav-menu");
-const body = document.querySelector("body");
+const burgurIcon = document.querySelector('.burgur-icon');
+const navMenu = document.querySelector('.nav-menu');
+const body = document.querySelector('body');
 
-
-
-burgurIcon.addEventListener("click", () => {
-    burgurIcon.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    body.classList.toggle("active");
-})
-
+burgurIcon.addEventListener('click', () => {
+  burgurIcon.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  body.classList.toggle('active');
+});
