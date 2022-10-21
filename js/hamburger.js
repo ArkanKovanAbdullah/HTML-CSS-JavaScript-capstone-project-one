4564ae102844a7ec687124308af4c7a888313a73
ArkanKovanAbdullah <79310452+ArkanKovanAbdullah@users.noreply.github.com>
2022-10-21 21:23:26 +0300

    Made the full website

---
+++ js/hamburger.js
@@ -0,0 +1,13 @@
+
+const burgurIcon = document.querySelector(".burgur-icon");
+const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector("body");
+
+
+
+burgurIcon.addEventListener("click", () => {
+    burgurIcon.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    body.classList.toggle("active");
+})
+
